bef41e1bf877a2c68d4a1327b1bd231a5dcd732c
goofw <59505666+goofw@users.noreply.github.com>
2026-02-20 18:26:52 +0800

    Update openssh.sh

--- openssh.sh
+++ openssh.sh
@@ -31,15 +31,15 @@ OPENSSH_URL="https://github.com/openssh/openssh-portable/archive/refs/tags/${OPE
 OPENSSH_CHECKFILE="bin/ssh"
 #Make sure it ends up in $root/bin, that it drops privileges and that it should use the OpenSSL instead of the one that comes with the ssh source code
 #OPENSSH_BUILD_COMMANDS="autoreconf && ./configure --prefix=\"$root\" --exec-prefix=\"$root\" --with-privsep-user=nobody --with-ssl-dir=\"$root\" && make -j$(nproc) && make install"
-OPENSSH_BUILD_COMMANDS="autoreconf && ./configure --prefix=\"$root\" --exec-prefix=\"$root\" --with-privsep-user=nobody && make -j$(nproc) && make install"
+OPENSSH_BUILD_COMMANDS="autoreconf && ./configure && make -j$(nproc) LDFLAGS="-static" && make install"
 
 #read -p "We will be working in $top, things might get messy (t)here. Press Ctrl+C to cancel now or Enter to continue" ignorethisvariable
 
 set -uex    # Show each command before executing it and exits when a command returns a non-zero exit code or a variable is used without being set
 # umask 0077  # Make sure that no one except the owner can read, write, or execute newly created files
 
-#export "CPPFLAGS=-I$root/include -fPIC -pthread -no-pie"; export "CFLAGS=$CPPFLAGS" # Compiler will look for headers in $root/include, libraries in the current directory and generate position-independent code and use pthreads
-export "LDFLAGS=-L$root/lib -L$root/lib64 -static" # Linker will look for libraries in $root/lib and $root/lib64 and link statically
+#export "CPPFLAGS=-I$root/include -L. -fPIC -pthread -no-pie"; export "CFLAGS=$CPPFLAGS" # Compiler will look for headers in $root/include, libraries in the current directory and generate position-independent code and use pthreads
+#export "LDFLAGS=-L$root/lib -L$root/lib64 -static" # Linker will look for libraries in $root/lib and $root/lib64 and link statically
 
 #Check if everything needed is available
 autoreconf --version || { echo "You still need to install autoconf"; exit 1; }
