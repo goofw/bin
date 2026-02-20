ed8d6c8d658a03c956c821d4e81d84ef99d1473a
goofw <59505666+goofw@users.noreply.github.com>
2026-02-20 19:22:30 +0800

    Update openssh.sh

--- openssh.sh
+++ openssh.sh
@@ -19,9 +19,9 @@ git checkout $(git tag --sort=-creatordate | grep -E "^V_\d+_\d+_P\d+$" | head -
 # https://stackoverflow.com/a/59473090
 export CC="cc -no-pie"
 # export CFLAGS="-no-pie"
-# export LDFLAGS="-static"
-./configure LDFLAGS="-static"
-make -j$(nproc) LDFLAGS="-static"
+export LDFLAGS="-static"
+./configure #LDFLAGS="-static"
+make -j$(nproc) #LDFLAGS="-static"
 
 
 find . -maxdepth 1 -type f -executable
