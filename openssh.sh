f936f75f9d94f77d16ae6dfa5af85709e8ae8b16
goofw <59505666+goofw@users.noreply.github.com>
2026-02-20 20:26:21 +0800

    Update openssh.sh

--- openssh.sh
+++ openssh.sh
@@ -5,7 +5,6 @@
 # https://github.com/openssh/openssh-portable#building-portable-openssh
 # https://gist.github.com/fumiyas/b4aaee83e113e061d1ee8ab95b35608b
 apk add \
-    clang \
     git \
     build-base \
     openssl-dev \
@@ -18,12 +17,10 @@ cd openssh-portable
 git checkout $(git tag --sort=-creatordate | grep -E "^V_\d+_\d+_P\d+$" | head -1)
 
 # https://stackoverflow.com/a/59473090
-# export CC=clang
-# export CC="cc -no-pie"
-#export CFLAGS="-no-pie"
-#export LDFLAGS="-L. -Lopenbsd-compat/ -static"
-export LDFLAGS="-static -no-pie"
-./configure --with-pie=no #LDFLAGS="-static"
+
+export CC="cc -static -no-pie"
+# export LDFLAGS="-static -no-pie"
+./configure --with-pie=no
 make -j$(nproc)
 
 
