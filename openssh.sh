467d6a63e3bef8d281ae6e45dc73328ef1e5033b
goofw <59505666+goofw@users.noreply.github.com>
2026-02-20 17:08:52 +0800

    Update openssh.sh

--- openssh.sh
+++ openssh.sh
@@ -37,7 +37,7 @@ OPENSSH_BUILD_COMMANDS="autoreconf && ./configure --prefix=\"$root\" --exec-pref
 set -uex    # Show each command before executing it and exits when a command returns a non-zero exit code or a variable is used without being set
 # umask 0077  # Make sure that no one except the owner can read, write, or execute newly created files
 
-export "CPPFLAGS=-I$root/include -L. -fPIC -pthread"; export "CFLAGS=$CPPFLAGS" # Compiler will look for headers in $root/include, libraries in the current directory and generate position-independent code and use pthreads
+export "CPPFLAGS=-I$root/include -L. -fPIC -pthread -no-pie"; export "CFLAGS=$CPPFLAGS" # Compiler will look for headers in $root/include, libraries in the current directory and generate position-independent code and use pthreads
 export "LDFLAGS=-L$root/lib -L$root/lib64 -static" # Linker will look for libraries in $root/lib and $root/lib64 and link statically
 
 #Check if everything needed is available
